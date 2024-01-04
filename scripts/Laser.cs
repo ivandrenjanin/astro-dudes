d15b1d1451455c1e21084480c36426a8ce97adf9
Ivan Drenjanjn <drenjanin.i@gmail.com>
2024-01-04 22:20:58 +0100

    fix: linting;

--- scripts/Laser.cs
+++ scripts/Laser.cs
@@ -6,7 +6,6 @@ public partial class Laser : Area2D
 	[Export]
 	public float Speed { get; set; } = 600.0f;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 newPosition = GlobalPosition;
